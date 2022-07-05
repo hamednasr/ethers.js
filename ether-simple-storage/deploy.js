2d6a61ccee74366236bc61b5afe74ee85448f440
hamed nasr <hmd.nasr@gmail.com>
2022-07-05 04:14:44 -0400

    deploy 3

--- ether-simple-storage/deploy.js
+++ ether-simple-storage/deploy.js
@@ -10,7 +10,19 @@ async function main() {
     "ce641621ce3a61940683a40d2e23940a79bd0beaa7b1ff28e72b9170cdf16e9e",
     provider
   );
+
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
+  const binary = fs.readFileSync(
+    "./SimpleStorage_sol_SimpleStorage.bin",
+    "utf8"
+  );
+
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying, please wait for a while...!");
+  const contract = await contractFactory.deploy();
+  console.log(contract);
 }
+// git push -u origin main
 
 main()
   .then(() => process.exit(0))
