e4440f3e9374e69e928f2c08731fbd9df2433237
hamed nasr <hmd.nasr@gmail.com>
2022-07-05 13:10:42 -0400

    manage private key

--- ether-simple-storage/deploy.js
+++ ether-simple-storage/deploy.js
@@ -1,46 +1,33 @@
 const ethers = require("ethers");
 const fs = require("fs-extra");
+require("dotenv").config();
 
 async function main() {
-  const provider = new ethers.providers.JsonRpcProvider(
-    "http://192.168.0.102:8545"
-  );
+  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
 
-  const wallet = new ethers.Wallet(
-    "178f966e1fadc7898ec54072ff15d0b8f5e7d8e9068f583572e4934431ad6da9",
-    provider
-  );
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
   const binary = fs.readFileSync(
     "./SimpleStorage_sol_SimpleStorage.bin",
     "utf8"
   );
-  // const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
-  // console.log("Deploying, please wait for a while...!");
-  // const contract = await contractFactory.deploy();
-  // const transactionReceipt = await contract.deployTransaction.wait(1);
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying, please wait for a while...!");
+  const contract = await contractFactory.deploy();
+  await contract.deployTransaction.wait(1);
 
-  console.log("lets only deploy with transaction data:....");
+  const currentFavouriteNumber = await contract.retrieve();
+  console.log(
+    `current fvourite number is: ${currentFavouriteNumber.toString()}`
+  );
 
-  const nonce = await wallet.getTransactionCount();
+  const transactionResponce = await contract.store("57892635343");
+  const transactionReceipt = transactionResponce.wait(1);
 
-  const tx = {
-    nonce: nonce,
-    gasPrice: 20000000000,
-    gasLimit: 1500000,
-    to: null,
-    value: 0,
-    data:
-      "0x" + fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf8"),
-    chainId: 1337,
-  };
-
-  const sentTxResponse = await wallet.sendTransaction(tx);
-  await sentTxResponse.wait(1);
-  console.log(sentTxResponse);
+  const newtFavouriteNumber = await contract.retrieve();
+  console.log(`new fvourite number is: ${newtFavouriteNumber.toString()}`);
 }
-// git push -u origin main
 
 main()
   .then(() => process.exit(0))
