663212c6b396600547a1011745d5a7316877bb88
hamed nasr <hmd.nasr@gmail.com>
2022-07-05 04:08:37 -0400

    deploy 2

--- ether-simple-storage/deploy.js
+++ ether-simple-storage/deploy.js
@@ -5,6 +5,11 @@ async function main() {
   const provider = new ethers.providers.JsonRpcProvider(
     "HTTP://127.0.0.1:7545"
   );
+
+  const wallet = new ethers.Wallet(
+    "ce641621ce3a61940683a40d2e23940a79bd0beaa7b1ff28e72b9170cdf16e9e",
+    provider
+  );
 }
 
 main()
