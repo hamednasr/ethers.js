3c34c5b2509c28f3ec140f95bacf3698c1e62884
hamed nasr <hmd.nasr@gmail.com>
2022-07-09 02:30:23 -0400

    test

--- hardhat_simple_storage/test/test-deploy.js
+++ hardhat_simple_storage/test/test-deploy.js
@@ -8,10 +8,18 @@ describe("SimpleStorage", () => {
     simpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
     simpleStorage = await simpleStorageFactory.deploy()
   })
+
   it("should start with a favorite number of 0", async () => {
     const currentValue = await simpleStorage.retrieve()
     const expectedValue = "0"
-
     assert.equal(currentValue.toString(), expectedValue)
+  })
+
+  it("should update when we call store", async () => {
+    const transactionResponse = await simpleStorage.store("5")
+    await transactionResponse.wait(1)
+    const newValue = await simpleStorage.retrieve()
+    const expectedValue = "5"
+    assert.equal(newValue.toString(), expectedValue)
   })
 })
