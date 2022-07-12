65fe927e886f7ad3ea757d212ba1f59a3f0ed71e
hamed nasr <hmd.nasr@gmail.com>
2022-07-12 00:20:54 -0400

    import ethers

--- html_fund_me/index.js
+++ html_fund_me/index.js
@@ -1,7 +1,14 @@
+import { ethers } from "./ethers-5.6.esm.min.js";
+
 const connect = async () => {
   if (typeof window.ethereum != "undefined") {
-    window.ethereum.request({ method: "eth_requestAccounts" });
-  } else console.log("metamask not installed!");
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+    document.querySelector("#connectButton").innerHTML = "Connected";
+  } else
+    document.querySelector("#connectButton").innerHTML = "Install Metamask";
 };
 
-connect;
+const fund = async (ethAmount) => {
+  if (typeof window.ethereum != "undefined") {
+  }
+};
