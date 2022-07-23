89c4b306c509d899c89947fa5c9f6184ceba6bb8
hamed nasr <hmd.nasr@gmail.com>
2022-07-23 07:44:31 -0400

    deploy mocks 1

--- hardhat_fund_me/deploy/01-deploy-fundme.js
+++ hardhat_fund_me/deploy/01-deploy-fundme.js
@@ -6,8 +6,18 @@
 
 // module.exports.default = deployFunc
 
+const { networkConfig } = require("../helper-hardhat-config.js")
+const { network } = require("hardhat")
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
-    const { deplyer } = await getNamedAccounts()
+    const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
+    const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: [ethUsdPriceFeedAddress],
+        log: true
+    })
 }
